dcdced006d052934fae2f581c560364ba23c3468
Renato Ramiro <renato.ipj@gmail.com>
2014-07-08 16:00:29 -0300

    Exibindo coleção de posts a partir do mongodb

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,26 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://discovermeteor.com'
-	},
-	{
-		title: 'coddesign',
-		author: 'Renato Ramiro',
-		url: 'http://coddesign.co'
-	},
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function () {
+		return Posts.find();
+	}
 });
